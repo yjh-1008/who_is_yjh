d293fbf9a0e1bea8946fdc7bbbca803e5ed9b6a2
yjh1008 <exesea12@gmail.com>
2023-11-02 01:17:05 +0900

    fixed : Badge 수정

--- src/components/MyBadges.vue
+++ src/components/MyBadges.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="badge_container">
-    <a
-      class="stack"
-      href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <img
-        src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-        alt="javascript"
-        width="40"
-        height="40"
-      />
-    </a>
+  <v-list width="300" v-model="open" bg-color="grey-lighten-3">
+    <v-list-group value="stacks">
+      <template v-slot:activator="{ props }">
+        <v-list-item v-bind="props"> My Stacks </v-list-item>
+      </template>
+      <v-list-item
+        v-for="item in items"
+        :key="item.name"
+        @click="onMove(item.href)"
+      >
+        <div class="d-flex">
+          <img :src="item.src" />
+          {{ item.name }}
+        </div>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
+  <!-- <div class="badge_container">
+
     <a
       href="https://reactjs.org/"
       target="_blank"
@@ -26,47 +31,64 @@
         height="40"
       />
     </a>
-    <a href="https://spring.io/" target="_blank" rel="noreferrer" class="stack">
-      <img
-        src="https://www.vectorlogo.zone/logos/springio/springio-icon.svg"
-        alt="spring"
-        width="40"
-        height="40"
-      />
-    </a>
-    <a
-      href="https://www.typescriptlang.org/docs/handbook/compiler-options.html"
-      target="_blank"
-      rel="noreferrer"
-      class="stack"
-    >
-      <img
-        src="https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg"
-        alt="typescript"
-        width="40"
-        height="40"
-      />
-    </a>
-    <a
-      href="https://www.oracle.com/kr/java/"
-      target="_blank"
-      rel="noreferrer"
-      class="stack"
-    >
-      <img
-        src="https://www.vectorlogo.zone/logos/java/java-icon.svg"
-        alt="java"
-        width="40"
-        height="40"
-      />
-    </a>
-  </div>
+  </div> -->
 </template>
 
+<script setup lang="ts">
+import { ref } from "vue";
+interface itemType {
+  src: string;
+  href: string;
+  name: string;
+}
+const open = ref(false);
+const items = ref<itemType[]>([
+  {
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    name: "Javascript",
+  },
+  {
+    src: "https://www.vectorlogo.zone/logos/typescriptlang/typescriptlang-icon.svg",
+    href: "https://www.typescriptlang.org/docs/handbook/compiler-options.html",
+    name: "Typescript",
+  },
+  {
+    src: "https://www.vectorlogo.zone/logos/java/java-icon.svg",
+    href: "https://www.oracle.com/kr/java/",
+    name: "Java",
+  },
+  {
+    src: "https://www.vectorlogo.zone/logos/vuejs/vuejs-ar21.svg",
+    href: "https://vuejs.org/",
+    name: "Vue",
+  },
+  {
+    src: "https://www.vectorlogo.zone/logos/nuxtjs/nuxtjs-icon.svg",
+    href: "https://nuxt.com/",
+    name: "Nuxt",
+  },
+  {
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+    href: "https://reactjs.org/",
+    name: "React",
+  },
+  {
+    src: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg",
+    href: "https://spring.io/",
+    name: "Spring",
+  },
+]);
+
+const onMove = (href: string) => {
+  window.location.href = href;
+};
+</script>
+
 <style scoped>
-.stack {
-  width: 50px;
-  height: 50px;
+img {
+  width: 60px;
+  height: 60px;
   padding-right: 10px;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,46 +1,54 @@
 <template>
-  <v-container class="pa-0">
-    <v-row align="center" class="pl-auto"><h2>About me!</h2></v-row>
-    <v-row align="center" class="pl-auto">
-      <v-col cols="3" class="pa-0">
-        <img :src="profile_pircutre" alt="profile" class="profile" />
-      </v-col>
-      <v-col cols="9">
-        <h2>편리함을 주고싶은 개발자 유준호입니다.</h2>
-        <h3 class="py-3">My Stacks</h3>
-        <MyBadges />
-        <ul>
-          <li>
-            Typescript, Jacascript, React, Vue를 주로 사용하는 프론트엔드
-            개발자입니다.
-          </li>
-          <li>Next, Nuxt 등 SSR에도 관심이 많아 공부하고 있습니다.</li>
-          <li>
-            반응형 UI/UX에 관심이 많고, 유저에게 더 편리한 사용을 위하여 항상
-            고민합니다!
-          </li>
-        </ul>
-      </v-col>
-    </v-row>
-    <h2>My Commits</h2>
-    <v-row class="py-5">
-      <img src="https://ghchart.rshah.org/yjh-1008" class="github_commits" />
-    </v-row>
-    <v-row>
-      <div>
-        Show My Github Stack!
-        <v-icon icon="mdi-chevron-down" />
-      </div>
-    </v-row>
-    <v-row>
-      <GithubStacks />
-    </v-row>
-  </v-container>
+  <div class="d-flex">
+    <v-card height="100vh" class="flex-1-0">
+      <v-container class="mr-10 mt-10">
+        <v-row class="pl-auto"><h2>About me!</h2></v-row>
+        <v-row class="pl-auto">
+          <v-col cols="9">
+            <h2>편리함을 주고싶은 개발자 유준호입니다.</h2>
+            <MyBadges />
+            <ul>
+              <li>
+                Typescript, Jacascript, React, Vue를 주로 사용하는 프론트엔드
+                개발자입니다.
+              </li>
+              <li>Next, Nuxt 등 SSR에도 관심이 많아 공부하고 있습니다.</li>
+              <li>
+                반응형 UI/UX에 관심이 많고, 유저에게 더 편리한 사용을 위하여
+                항상 고민합니다!
+              </li>
+            </ul>
+          </v-col>
+        </v-row>
+        <h2>My Commits</h2>
+        <v-row class="py-5">
+          <img
+            src="https://ghchart.rshah.org/yjh-1008"
+            class="github_commits"
+          />
+        </v-row>
+        <v-row>
+          <div>
+            Show My Github Stack!
+            <v-icon icon="mdi-chevron-down" />
+          </div>
+        </v-row>
+        <v-row>
+          <GithubStacks />
+        </v-row>
+      </v-container>
+    </v-card>
+    <v-card width="50%">
+      <div>section 1</div>
+    </v-card>
+    <v-card width="50%">
+      <div>section 1</div>
+    </v-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref, Ref } from "vue";
-import { loadFile } from "@/plugins/firebase";
 import MyBadges from "@/components/MyBadges.vue";
 import GithubStacks from "@/components/GithubStacks.vue";
 const profile_pircutre: Ref<any> = ref("");
