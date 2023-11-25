de9395880f6f2e0b720c7439dd15e972f0b0d550
yjh1008 <exesea12@gmail.com>
2023-11-25 11:25:56 +0900

    fixed : contentItem의 css 변경.

--- src/components/ContentItem.vue
+++ src/components/ContentItem.vue
@@ -1,27 +1,28 @@
 <template>
   <v-overlay v-if="!content" />
   <template v-else>
-    <v-card
-      density="comfortable"
-      fill-height
-      class="mx-auto w-100 viewer-title"
-    >
+    <v-card density="comfortable" fill-height class="mx-auto w-100">
       <v-card-title>
         <div class="d-flex justify-center">
-          <div class="title w-75 mx-auto">
-            <div class="title-text">
+          <div class="title w-100 mx-auto">
+            <div class="title-text py-3">
               {{ content.title }}
+            </div>
+            <div class="date">
+              {{ date }}
             </div>
           </div>
         </div>
       </v-card-title>
       <v-card-subtitle>
-        <div class="w-100 mb-3 d-flex mx-auto">
+        <div class="w-100 mb-3 mx-auto">
           <v-expansion-panels>
             <v-expansion-panel>
-              <v-expansion-panel-title>Panel 1</v-expansion-panel-title>
-              <v-expansion-panel-text>
-                <div>카테고리: {{ content.category }}</div>
+              <v-expansion-panel-title
+                ><div>카테고리: {{ content.category }}</div>
+              </v-expansion-panel-title>
+
+              <v-expansion-panel-text class="d-flex">
                 <v-chip-group>
                   <v-chip v-for="chip in content.tags" :key="chip">{{
                     chip
@@ -50,11 +51,12 @@ const props = defineProps<{
   id: string;
 }>();
 const content = ref<Content | null>();
-
+const date = ref<string>();
 onMounted(async () => {
-  console.log(props.id);
   return getPost(props.id).then((data) => {
-    // console.log(data);
+    if (data.createdAt !== null || data.createdAt !== undefined) {
+      date.value = `${data.createdAt?.getFullYear()}년 ${data.createdAt?.getMonth()} 월 ${data.createdAt?.getDate()}일`;
+    }
     content.value = data;
   });
 });
@@ -69,10 +71,22 @@ const onModify = () => {
   text-align: center;
 }
 .title {
-  width: 500px;
+  width: 100%;
+  border: 1px solid #d3d3d3;
+  min-height: 100px;
+  padding: 1%;
 }
 .title-text {
   font-weight: bold;
-  font-size: 3rem;
+  font-size: 2rem;
+}
+
+.date {
+  color: #d3d3d3;
+  font-size: 1rem;
+}
+
+.v-expansion-panel-text {
+  background-color: lightgray;
 }
 </style>
